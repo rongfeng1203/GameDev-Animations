6379aa26605b68f25b437b93fffa7da8b6189052
rongfeng1203 <rongfeng1203@gmail.com>
2026-02-26 11:46:57 -0500

    yas

---
+++ 01-intro_to_p5.js/sum.py
@@ -0,0 +1,36 @@
+SUM = eval(input("Enter a sum: "))
+PROD = eval(input("Enter a product: "))
+Vsum = True
+Vproduct = True
+if PROD < 0:
+    if PROD < 0:
+        Vproduct = False
+        if SUM < 0:
+            Vsum = False
+i = 0
+factors = []
+for _ in range(int(product**0.5)):
+    if product%(i+1) == 0:
+        factors.append(((i+1),(product//(i+1))))
+if Vproduct == True:
+    for i in range(len(factors)): #should be a for-each loop
+        "add factor pairs together"
+        if sum_factor_pair == abs(SUM):
+            if Vsum == True:
+                "output factor pair"
+            elif Vsum == False:
+                "output factor pair with - sign infront both #s"
+            else:
+                print("Not possible.")
+if Vproduct == False:
+    for i in range(len(factors)): #also should be a for-each
+        if Vsum == False:
+            "make 2nd number of factor pair negative"
+            "add them together (store in 'result')"
+            if result == SUM:
+                "unspecified"
+        if Vsum == True:
+            "make 2nd number of factor pair negative"
+            "add them together (store in 'result')"
+            if result == SUM:
+                "unspecified"
